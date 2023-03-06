f58120b663c6a9b35bd7f9ec46fce0f643ce1186
dev-sincerity <kllliv50@gmail.com>
2023-03-06 10:22:33 +0900

    최초커밋

---
+++ bitbucket-jenkins-repo/src/main/java/com/example/bitbucketjenkinsrepo/BitbucketJenkinsRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitbucketjenkinsrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbucketJenkinsRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitbucketJenkinsRepoApplication.class, args);
+    }
+
+}
---
+++ bitbucket-jenkins-repo/src/test/java/com/example/bitbucketjenkinsrepo/BitbucketJenkinsRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitbucketjenkinsrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitbucketJenkinsRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
